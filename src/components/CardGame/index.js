ac30ab4dff4b15fcb8b7819af8cc34c2cbd6c657
Danilo Fontes <slipdan@hotmail.com>
2021-07-09 17:40:41 -0300

    Update CardGame component

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,11 +1,12 @@
-import "./style.css"
+import "./style.css";
+import "../../styles/settings/colors.css";
 
 function CardGame() {
-   return /*html*/`
-    <figure class="card-game">
-        <img src="images/alura-pixel.svg" alt="Logo da Alura" class="icon">>
-    </figure>
-    `;
+    return /*html*/`
+        <figure class="card-game">
+            <img src="images/alura-pixel.svg" alt="Logo da Alura" class="icon">
+        </figure>        
+        `;           
 }
 
 export default CardGame;
