df6ab9fa8b91c98e50c105e5588ab75de43ee554
Danilo Fontes <slipdan@hotmail.com>
2021-07-26 22:32:39 -0300

    Update imports of styles

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 import "./src/styles/settings/colors.css";
-import "./src/styles/generic/reset.css"
 import "./src/styles/tools/NessBorder/style.css";
 import BoardGame from "./src/objects/BoardGame";
 import PlayerName from "./src/components/PlayerName";
