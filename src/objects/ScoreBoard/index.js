56d978045ada0da7e6f113cddb56958ab93dcd2d
Danilo Fontes <slipdan@hotmail.com>
2021-08-02 14:54:47 -0300

    Update ScoreBoard with Vs component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import PlayerName from "../../components/PlayerName";
+import Vs from "../../components/Vs";
 import './style.css'
 
 function ScoreBoard() {
     return /*html*/ `
         <header class="score-board">
             ${PlayerName('Player1')}
+            ${Vs("vs")}
             ${PlayerName('Player2')}
         </header>
     `;
