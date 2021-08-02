93e3d3aa96bbafcf8316f12e2b41f9a587902ec9
Danilo Fontes <slipdan@hotmail.com>
2021-08-02 18:36:22 -0300

    Update ScoreBoard object with PlayerPoints

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,12 +1,15 @@
 import PlayerName from "../../components/PlayerName";
+import PlayerScore from "../../components/Playerscore";
 import Vs from "../../components/Vs";
-import './style.css'
+import './style.css';
 
 function ScoreBoard() {
     return /*html*/ `
         <header class="score-board">
-            ${PlayerName('Player1')}
+            ${PlayerName('Player1')}            
+            ${PlayerScore()}
             ${Vs("vs")}
+            ${PlayerScore()}
             ${PlayerName('Player2')}
         </header>
     `;
