c83062587184330a9a5c3c2657398e2f6cb1571c
Danilo Fontes <slipdan@hotmail.com>
2021-10-20 23:35:34 -0300

    Create component player turn

---
+++ src/components/PlayerTurn/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+function PlayerTurn() {
+    return /*html*/ `
+    <div class="player-turn">
+        <img src="images/IconArrowDown.png" alt="Icon that shows who that has the turn" class="icon">        
+    </div>
+    `
+}
+
+export default PlayerTurn;
