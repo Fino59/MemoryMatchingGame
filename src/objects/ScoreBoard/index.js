cf4be11089e25f586c0ea570224ffbaabb79ed60
Danilo Fontes <slipdan@hotmail.com>
2021-10-26 22:10:02 -0300

    Add Arrow Down component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,13 @@
 import PlayerName from "../../components/PlayerName";
 import PlayerScore from "../../components/PlayerScore";
 import Vs from "../../components/Vs";
+import ArrowDown from "../../components/ArrowDown"
 import './style.css';
 
 function ScoreBoard() {
     return /*html*/ `
         <header class="score-board">
+            ${(ArrowDown())}
             ${PlayerName('Player1')}            
             ${PlayerScore(1)}
             ${Vs("vs")}
