d28a82966a26212426117ec7414bac335331f558
Danilo Fontes <slipdan@hotmail.com>
2021-10-26 22:10:39 -0300

    Update Playerscore with css

--- src/components/Playerscore/index.js
+++ src/components/Playerscore/index.js
@@ -2,10 +2,10 @@ import './style.css';
 
 function PlayerScore(points = 0) {    
     return /*html*/ `
-        <ol data-points="${points}" >
-            <li>I</li>
-            <li>II</li>
-            <li>III</li>
+        <ol class="player-score" data-points="${points}" >
+            <li class="pointer">I</li>
+            <li class="pointer">II</li>
+            <li class="pointer">III</li>
         </ol>
     `;
 }
