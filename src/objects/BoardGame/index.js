64cab1383d9da18c60d6db5daa49a40c14dd59b0
Danilo Fontes <slipdan@hotmail.com>
2021-11-24 23:29:23 -0300

    Update BoardGame with function swapPlayers

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -4,12 +4,34 @@ import CardFrontBack from "../../components/CardFrontBack";
 import cards from "./data.js";
 
 function BoardGame(amountCards) {    
-    
+    const flipAndHideCards = ($cardsActive) => {
+        $cardsActive.forEach((card) => card.classList.remove('-active'));
+    }
+
+    const swapPlayer = () => {
+        const $arrowDown = document.querySelector('.arrow-down');
+        const currentplayer = $arrowDown.getAttribute('data-currentplayer');
+        $arrowDown.setAttribute('data-currentplayer', currentplayer == 1 ? 2 : 1);
+    }
+
+    window.boardGame = {};
+    window.boardGame.handleClick = () => {        
+        const $boardGame = document.querySelector('.board-game');
+        
+        const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active');
+
+        if ($cardsActive.length === 2) {
+            setTimeout(() => {                
+                flipAndHideCards($cardsActive);
+                swapPlayer();
+            }, 800)
+        }
+    }
     const $htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
     const $htmlCards = $htmlCardsList.join('')
 
-    return `
-    <section class="board-game">
+    return /*html*/ `
+    <section class="board-game" onClick="boardGame.handleClick()">
         ${$htmlCards}       
     </section>
     `; 
